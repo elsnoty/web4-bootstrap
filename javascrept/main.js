b6b0dc18ce7279b610d98c6221d8c88e79198880
MohamedEmadHamida <128223543+MohamedEmadHamida@users.noreply.github.com>
2023-12-16 18:24:12 +0200

    first commit

---
+++ javascrept/main.js
@@ -0,0 +1,33 @@
+function CopyCoupon() {
+  navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
+    if (result.state === 'granted' || result.state === 'prompt') {
+      navigator.clipboard.writeText("Coupon50%OFF").then(() => {
+        // Alert the copied text
+        alert("Copied: Coupon50%OFF");
+
+        // Change the color of the element
+        var couponcolor = document.getElementById("copy-coupon");
+        couponcolor.style.color = "rgb(167, 243, 208)";
+      }).catch(error => {
+        console.error('Failed to copy text:', error);
+      });
+    }
+  });
+}
+
+/* old code */
+/*
+CopyCoupon() {
+//copy coipon
+navigator.clipboard.writeText("Coupon50%OFF");
+// Alert the copied text
+alert("Copied : " + "Coupon50%OFF");
+var couponcolor = document.getElementById("copy-coupon");
+couponcolor.style.color="rgb(167,243,208)";
+}
+
+
+*/
+
+var couponcolor = document.getElementById("copy-coupon");
+couponcolor.style.color="rgb(167,243,208)";
